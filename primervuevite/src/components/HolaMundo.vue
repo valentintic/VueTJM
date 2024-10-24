c851d66baf38496720b56f8d3830f9fd52bbbd6a
valentintic <valentinpreuteseitic@gmail.com>
2024-10-24 14:07:05 +0200

    first Commit

---
+++ primervuevite/src/components/HolaMundo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Mi primera App de Vue</h1>
+    </div>
+</template>
+
+<script>
+    // Esto es codigo js (especial)
+    // cada component que creemos siempre debemos exportarlo
+    // Con un name
+    export default {
+        name: "HolaMundo",
+    }
+</script>
+
+<style>
+    h1 {
+        color:red
+    }
+</style>
